34b7a4443c860ab923d36d6e1cc2779a6e7767e4
Chiyu Jiang <maxjiang>
2020-04-22 22:07:20 -0700

    add download thumbnail

--- download_data.sh
+++ download_data.sh
@@ -7,12 +7,14 @@ echo "Downloading and unzipping data. This will take a while, take a break and g
 echo "Downloading Data..."
 wget island.me.berkeley.edu/datasets/shapenet_chair_watertight.zip .
 wget island.me.berkeley.edu/datasets/shapenet_chair_simplified.zip .
+wget island.me.berkeley.edu/datasets/shapenet_chair_thumbnails.zip .
 
-mv shapenet_chair_watertight.zip shapenet_chair_simplified.zip data
+mv shapenet_chair_watertight.zip shapenet_chair_simplified.zip shapenet_chair_thumbnails.zip data
 cd data
 echo "Unzipping Data..."
 unzip shapenet_chair_watertight.zip
 unzip shapenet_chair_simplified.zip
+unzip shapenet_chair_thumbnails.zip
 
 rm *.zip
 cd ..
